044664b4aaa513d81f08a3e3036e1a1c0c1e6fc8
youfeed <11247005@qq.com>
2024-05-07 02:53:49 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 import {resolve} from 'path'
 export default defineConfig({
-  plugins: [vue(),UnoCSS()],
+  plugins: [vue(),UnoCSS(),viteExternalsPlugin({
+    vue: 'Vue'
+  })],
   build:{
     cssCodeSplit:false,
     modulePreload:{ polyfill:false },
