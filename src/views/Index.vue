7980eae72a69d7158689c70d7e98912c0772acc5
youfeed <11247005@qq.com>
2024-09-26 03:32:00 +0800

    add

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,24 +1,14 @@
 <template>
   <div>
-    <div>233</div>
     <component :is="currentComponent"></component>
   </div>
 </template>
 <script setup>
 import IndexHome from './IndexHome.vue'
 import IndexConsole from './IndexConsole.vue'
-// import { onMounted,computed } from "vue"
-
-
 const currentComponent = computed(() => {
   let {uuid} = useAuth()
   return uuid ? IndexConsole : IndexHome;
-});
-// 
-onMounted(()=>{
-
-  
-  // observer.observe(state.ref) 
 });
 </script>
 
