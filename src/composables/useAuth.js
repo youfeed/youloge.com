615adac13949c280c4486acde140474cb9b8e036
youfeed <11247005@qq.com>
2024-10-04 02:15:14 +0800

    add

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,7 +1,7 @@
 export default ()=>{
   try {
       const {expire=0,...profile} = useStorage('profile'),timed = new Date().getTime() / 1000 >> 0;
-      return expire < timed ? (profile.expire = expire,profile) : {};
+      return expire > timed ? (profile.expire = expire,profile) : {};
     } catch (error) {
       return {}
     }
