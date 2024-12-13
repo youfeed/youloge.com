5fa6971b4213d0e1ba9a91ec2bef3a24c676c0a5
youfeed <11247005@qq.com>
2024-12-14 05:46:07 +0800

    add

--- src/console/Article.vue
+++ src/console/Article.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-screen-md mx-auto p-4">
     <div class="panel mb-4 bg-light p-2 rounded-sm">
-      <template v-if="mode == 'preview'">
+      <template v-if="state.mode == 'preview'">
         <div class="panel-head flex items-center justify-between  h-10 border-b-solid border-b border-gray-200">
           <div class="flex items-center gap-5">
             <div>文章</div>
@@ -21,7 +21,7 @@
               <div class="flex justify-between items-center">
                 <div class="text-sm text-gray-500">{{ item.created }} · {{ item.view }} · {{ item.uuid }}</div>
                 <template v-if="item.status == 0">
-                  <div class="text-sm text-gray-500" @click="navigateTo('articledraft',item.uuid)">编辑</div>
+                  <div class="text-sm text-gray-500" @click="onEditor(item)">编辑</div>
                 </template>
                 <template v-else-if="item.status == 1">
                   <div class="text-sm text-gray-500">浏览</div>
@@ -38,16 +38,22 @@
       
       
         
-        <template v-if="mode == 'editor'">
+        <template v-if="state.mode == 'draft'">
           <div class="panel-head flex items-center justify-between  h-10 border-b-solid border-b border-gray-200">
             <div class="flex items-center gap-5">
               <div>编辑模式</div>
             </div>
-            <div>保存草稿</div>
+            <div @click="onSave">保存草稿</div>
           </div>
           <div class="panel-body pt-2">
-            <YouEditor v-model="html"></YouEditor>
-            <button class="" @click="saveReadme">记得保存</button>
+            <div class="metadata">
+              <div class="poster"></div>
+              <div class="title"></div>
+              <div class="keywords"></div>
+              <div class="description"></div>
+            </div>
+            <YouEditor v-model="state.draft"></YouEditor>
+            <button class="" >记得保存</button>
           </div>
         </template>
         
@@ -63,8 +69,9 @@
 <script setup>
 let params = defineProps(['params']);const emit = defineEmits(['jump']);
 const state = reactive({
-  err:0,msg:'',data:{},
-  profile:{}
+  err:0,msg:'',data:{},profile:{},
+  mode:'preview',// preview draft
+  draft:'',
 }),{err,msg,data,profile} = toRefs(state);
 //
 const loadArticle = ()=>{
@@ -78,6 +85,17 @@ const newDraft = ()=>{
     console.log(res)
   }).catch(err=>{});
   console.log('newDraft')
+}
+// 编辑草稿
+const onEditor = (item)=>{
+  state.mode = 'draft';
+  state.draft = '<p>111</p>';
+  console.log(item)
+}
+// 保存草稿
+const onSave = ()=>{
+  state.mode = 'preview';
+  console.log('onSave')
 }
 //
 onMounted(()=>{
