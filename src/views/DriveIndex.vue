682fa8b75043ecbd4de79ff76f36d16078680328
youfeed <11247005@qq.com>
2025-12-27 19:46:03 +0800

    啊福娃

--- src/views/DriveIndex.vue
+++ src/views/DriveIndex.vue
@@ -1,61 +1,80 @@
 <template>
-  <!-- 英雄区域 -->
-  <section class="bg-gradient-to-b from-blue to-white py-16 md:py-24">
-    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="max-w-4xl mx-auto text-center">
-        <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-secondary mb-6 leading-tight">
-          Youloge Drive
-        </h1>
-        <br class="md:hidden"></br>
-        <h3 class="text-xl font-bold text-secondary mb-6 leading-tight">
-          云存储资源分享<br class="md:hidden">+
-        </h3>
-        <!-- 搜索框 -->
-        <div class="max-w-80 mx-auto  mb-4">
-          <form action="/drive/search" method="get" class="relative flex items-center gap-2">
-            <input type="search" name="q" placeholder="搜索云盘资源"
-              class="border border-gray-300 rounded-full outline-none bg-transparent px-2 py-1 w-full" required>
-            <button type="submit" class="absolute inset-y-0 right-1 border-0 outline-0 bg-transparent text-blue-500">搜索</button>
-          </form>
+    <!-- 英雄区域 -->
+    <section class="bg-gradient-to-b from-blue to-white py-16 md:py-24">
+        <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="max-w-4xl mx-auto text-center">
+                <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-secondary mb-6 leading-tight">
+                    Youloge Drive
+                </h1>
+                <br class="md:hidden" />
+                <h3 class="text-xl font-bold text-secondary mb-6 leading-tight">
+                    云存储资源分享<br class="md:hidden">+
+                </h3>
+                <!-- 搜索框 -->
+                <div class="max-w-80 mx-auto  mb-4">
+                    <form action="/drive/search" method="get" class="relative flex items-center gap-2">
+                        <input type="search" name="q" placeholder="搜索云盘资源"
+                            class="border border-gray-300 rounded-full outline-none bg-transparent px-2 py-1 w-full"
+                            required>
+                        <button type="submit"
+                            class="absolute inset-y-0 right-1 border-0 outline-0 bg-transparent text-blue-500">搜索</button>
+                    </form>
+                </div>
+                <p class="text-xl text-gray-600 mb-2 text-balance">
+                    不限速度: 下载速度没有限制 享受CDN高速下载
+                </p>
+                <p class="text-xl text-gray-600 mb-2 text-balance">
+                    不限大小: 文件大小不限, 支持GB级别大文件
+                </p>
+                <p class="text-xl text-gray-600 mb-2 text-balance">
+                    下载次数: 24小时内不限次数下载
+                </p>
+                <!-- 快速统计 -->
+                <div class="flex flex-wrap justify-center gap-x-8 gap-y-2 text-sm text-gray-500">
+                    <div>分享资源</div>
+                </div>
+            </div>
         </div>
-        <p class="text-xl text-gray-600 mb-2 text-balance">
-          不限速度: 下载速度没有限制 享受CDN高速下载
-        </p>
-        <p class="text-xl text-gray-600 mb-2 text-balance">
-          不限大小: 文件大小不限, 支持GB级别大文件
-        </p>
-        <p class="text-xl text-gray-600 mb-2 text-balance">
-          下载次数: 24小时内不限次数下载
-        </p>
-        <!-- 快速统计 -->
-        <div class="flex flex-wrap justify-center gap-x-8 gap-y-2 text-sm text-gray-500">
-          <div>分享资源</div>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
+    <!-- 内容区域 -->
+    <div></div>
 </template>
 
 <script setup>
 import { reactive, toRefs, onMounted } from "vue"
 
 const state = reactive({
-  err: 0,
-  msg: '',
-  count: 0,
-  data: {},
-}), { err, msg, data } = toRefs(state);
+    err: 0,
+    msg: '',
+    count: 0,
+    lists: {},
+    cursor: {
+        limit: 10,
+        offset: 0,
+        status: '',
+        next_cursor: ''
+    },
+}), { err, msg, lists } = toRefs(state);
 //
 const onSearch = () => {
-  apiFetch('search/drive', {}).then(res => Object.assign(state, res)).catch((err) => {
-    console.log('err', state, err)
-
-  })
-  // console.log('err',state)
+    const { cursor } = state
+    apiFetch('search/drive', {
+        q: '*',
+        limit: cursor.limit,
+        offset: cursor.offset
+    }).then(({ hits, ...cursor }) => {
+        state.cursor = cursor;
+        hits.forEach(is => {
+            let inx = state.lists.findIndex(it => it.uuid == is.uuid);
+            inx == -1 && state.lists.push(is)
+        });
+        console.log(cursor)
+    }).catch((err) => {
+        console.log('err', state, err)
+    })
 }
 onMounted(() => {
-
-  onSearch();
+    onSearch();
 })
 </script>
 
--- src/views/VideoIndex.vue
+++ src/views/VideoIndex.vue
@@ -1,43 +1,72 @@
 <template>
-  <div id="container" class="m-2 sm:m-4 md:m-8">
-    <!-- 英雄区域 -->
-    <section class="bg-gradient-to-b from-blue to-white py-16 md:py-24">
-      <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="max-w-4xl mx-auto text-center">
-          <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-secondary mb-6 leading-tight">
-            视频短片<br class="md:hidden">+
-          </h1>
-          <!-- 搜索框 -->
-          <div class="max-w-80 mx-auto">
-            <form action="/video/search" method="get" class="flex gap-1">
-              <input type="search" name="q" placeholder="搜索视频资源" class="border rounded px-2 py-1 w-full" required>
-              <button type="submit" class=" px-1 py-1 w-15">搜索</button>
-            </form>
-          </div>
-          <p class="text-xl text-gray-600 mb-8 text-balance">
+    <div id="container" class="m-2 sm:m-4 md:m-8">
+        <!-- 英雄区域 -->
 
-          </p>
-          <!-- 快速统计 -->
-          <div class="flex flex-wrap justify-center gap-x-8 gap-y-2 text-sm text-gray-500">
-            <div @click="useKeyboard({ type: 'letter', prefix: 'XYZ' }).then(res => console.log(res))"><span
-                class="font-semibold text-secondary">100M+</span> 分享视频</div>
-          </div>
+        <!-- 宫格内容 -->
+        <div
+            class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 sm:gap-6">
+            <template v-for="item in lists" :key="item.uuid">
+                <div class="video-card group">
+                    <router-link :to="`/video/${item.uuid}`">
+                        <div class="relative rounded-lg overflow-hidden aspect-video bg-gray-100 mb-3">
+                            <img :src="useImage(item.poster,150)" :alt="item.title"
+                                class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105">
+                            <span
+                                class="video-duration absolute bottom-2 right-2 bg-black/80 text-white text-xs px-1.5 py-0.5 rounded">05:24</span>
+                            <div
+                                class="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center">
+                                <div
+                                    class="w-12 h-12 bg-primary rounded-full flex items-center justify-center transform translate-y-4 group-hover:translate-y-0 transition-transform">
+                                    <i class="fa fa-play text-white ml-1"></i>Play
+                                </div>
+                            </div>
+                        </div>
+                    </router-link>
+                    <router-link :to="`/video/${item.uuid}`"
+                        class="font-medium line-clamp-2 mb-1 group-hover:text-primary transition-colors">
+                        {{item.title}} </router-link>
+                    <div class="flex items-center text-sm text-gray-500">
+                        <span class="line-clamp-1">{{item.account.name}}</span>
+                        <span class="mx-2">•</span>
+                        <span>{{item.views}}次观看</span>
+                    </div>
+                </div>
+            </template>
         </div>
-      </div>
-    </section>
-  </div>
+    </div>
 </template>
 <script setup>
+import apiFetch from '../composables/apiFetch';
+
 const state = reactive({
-  list: [],
-  // 分页游标
-  cursor: {
-    status: '',
-    next_cursor: ''
-  },
-  title: 'Youloge 视频点播',
-  mode: 'preview',// preview draft
-}), { title} = toRefs(state);
+    lists: [],
+    // 分页游标
+    cursor: {
+        limit: 10,
+        offset: 0,
+        status: '',
+        next_cursor: ''
+    },
+    title: 'Youloge 视频点播',
+    mode: 'preview',// preview draft
+}), { title, lists } = toRefs(state);
+
+// 加载搜索
+const loadSearch = () => {
+    const { cursor } = state;
+    apiFetch('search/video', {
+        q: '*',
+        offset: cursor.offset,
+        limit: cursor.limit,
+    }).then(({ hits, ...cursor }) => {
+        state.cursor = cursor;
+        hits.forEach(is => {
+            let inx = state.lists.findIndex(it => it.uuid == is.uuid);
+            inx == -1 && state.lists.push(is)
+        });
+    })
+
+}
 
 
 const io = new IntersectionObserver((entries, observer) => {
@@ -45,8 +74,9 @@ const io = new IntersectionObserver((entries, observer) => {
 }, { rootMargin: '0px', threshold: 1.0 });
 //
 onMounted(() => {
-  document.title = state.title;
-  // io.observe(document.querySelector("#loading"));
+    document.title = state.title;
+    loadSearch();
+    // io.observe(document.querySelector("#loading"));
 });
 </script>
 
